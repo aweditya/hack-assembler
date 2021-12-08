353236161a2829ec6796504d3443119ac35096c8
aweditya <aweditya@gmail.com>
2021-12-08 14:59:52 +0530

    comp binary code mapping complete

--- src/Code.java
+++ src/Code.java
@@ -29,7 +29,37 @@ public class Code {
      * @return
      */
     public String comp(String mnemonic) {
-        return "comp";
+        return switch (mnemonic) {
+            case "0" -> "0101010";
+            case "1" -> "0111111";
+            case "-1" -> "0111010";
+            case "D" -> "0001100";
+            case "A" -> "0110000";
+            case "!D" -> "0001101";
+            case "!A" -> "0110001";
+            case "-D" -> "0001111";
+            case "-A" -> "0110011";
+            case "D+1" -> "0011111";
+            case "A+1" -> "0110111";
+            case "D-1" -> "0001110";
+            case "A-1" -> "0110010";
+            case "D+A" -> "0000010";
+            case "D-A" -> "0010011";
+            case "A-D" -> "0000111";
+            case "D&A" -> "0000000";
+            case "D|A" -> "0010101";
+            case "M" -> "1110000";
+            case "!M" -> "1110001";
+            case "-M" -> "1110011";
+            case "M+1" -> "1110111";
+            case "M-1" -> "1110010";
+            case "D+M" -> "1000010";
+            case "D-M" -> "1010011";
+            case "M-D" -> "1000111";
+            case "D&M" -> "1000000";
+            case "D|M" -> "1010101";
+            default -> "2222222";
+        };
     }
 
     /**
